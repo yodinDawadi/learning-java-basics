f81ba81c653630106d096dce54c1ab2cec8c40fc
Yodin dawadi <yodindawadi10@gmail.com>
2024-07-06 22:55:03 +0545

    change

--- 01/MyFirstProgram.java
+++ 01/MyFirstProgram.java
@@ -2,6 +2,7 @@
 public class MyFirstProgram {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.print("Hello world \n");
+        System.out.print("Im 18 years old");
     } 
 }
